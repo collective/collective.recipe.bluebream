c0aef4386607004d3cdc5ca7ac171f9bb73bcf46
Alex Clark <aclark@aclark.net>
2011-07-27 08:28:35 -0400

    Make it work

--- collective/recipe/bluebream/__init__.py
+++ collective/recipe/bluebream/__init__.py
@@ -1,14 +1,19 @@
-
-# This recipe currently does ``nothing``, except require Bluebream packages.
+# This recipe currently does ``nothing``[1] except require Bluebream packages.
 # In the future, it may do ``something``.
+
+# [1] Actually, it installs ``bin/paster``
+
+from zc.buildout.easy_install import scripts
+import pkg_resources
 
 class Recipe(object):
     
     def __init__(self, buildout, name, options):
-        pass
+        self.buildout = buildout
 
     def install(self):
-        pass
+        # Generate a paster script
+        return scripts(['PasteScript'], pkg_resources.working_set, None, self.buildout['buildout']['bin-directory'])
 
     def update(self):
         pass
