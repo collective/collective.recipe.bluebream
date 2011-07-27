06ffbe0259077482b7e68f4b2de4b96ae5d929cf
Alex Clark <aclark@aclark.net>
2011-07-27 07:32:54 -0400

    Mark it up

---
+++ collective/recipe/bluebream/__init__.py
@@ -0,0 +1,14 @@
+
+# This recipe currently does ``nothing``, except require Bluebream packages.
+# In the future, it may do ``something``.
+
+class Recipe(object):
+    
+    def __init__(self, buildout, name, options):
+        pass
+
+    def install(self):
+        pass
+
+    def update(self):
+        pass
